b93a8749fcc5a2662dc525f4dbd2dab0075d2003
vitor-portela-lopes <vitor.portela.lopes@escola.pr.gov.br>
2025-07-30 13:35:43 +0000

    aula 05

--- aula05.py
+++ aula05.py
@@ -8,3 +8,18 @@ dados = [10, 20, 20, 30, 40, 50, 50, 50, 60, 70, 80, 100]
 # Calcular quartis
 quartis = np.percentile(dados, [25,50,75])
 print(f'Quartis: Q1={quartis[0]}, Q2={quartis[1]}, Q3={quartis[2]}')
+
+# Calcular decis
+decis = np.percentile(dados, [10, 20, 30, 40, 50, 60, 70, 80, 90])
+print(f'Decis: {decis}')
+
+# Calcular percentis
+percentis = np.percentile(dados, [10, 25, 50, 75, 90])
+print(f'Percentis: {percentis}')
+
+# Visualização por BoxPlot
+plt.boxplot(dados, vert=False)
+plt.title('boxplot das notas')
+plt.xlabel('notas')
+plt.show()
+plt.savefig('chart5.png')
